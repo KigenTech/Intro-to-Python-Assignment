a41afd1a6260f4b1b41ac7684960c11da706211f
comrad <comrad@comrad>
2024-09-02 11:09:55 +0300

    initial-commit

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask for users name and store it in a variable called "name"
+name = input("What is your name? ")
+
+# Ask for users age and store it in a variable called "age"
+age = input("How old are you? ")
+
+# Ask for users location and store it in a variable called "location"
+location = input("Where do you live? ")
+
+# Print out a personalized message.
+print(f"Hello {name}, you are {age} years old and live in {location}.")
